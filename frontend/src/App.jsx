d0f1ded91c032558be153c5e292ada39a55c7f49
Greg-js1007 <suberog010@gmail.com>
2025-06-11 23:23:57 -0400

    agregando una carpeta styles junto a los css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
-import './App.css'
+import './styles/App.css'
 
 function App() {
 
   return (
     <>
-      
+      <main className='bg-amber-400'>
+        <h1>Bienvenido a nuestro proyecto</h1>
+      </main>
     </>
   )
 }
