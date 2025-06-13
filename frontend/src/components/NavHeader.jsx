6cfab8042c9e44f5b0f9936b4858768253458281
Gregory Subero <59521617+gr3g0r7sjr@users.noreply.github.com>
2025-06-13 19:09:31 +0000

    fix: Ajustando las medias del navHeader y agregando otro container

--- frontend/src/components/NavHeader.jsx
+++ frontend/src/components/NavHeader.jsx
@@ -6,26 +6,28 @@ export const NavHeader = () => {
     const imageNavBg = bgNav; 
     
     return (
-        <section className = "flex w-full py-2.5 px-5 items-center justify-between" style={{ backgroundImage: `url('${imageNavBg}')` }}>
-            <figure className="w-32">
-                <img src={novumLogo} alt="Logo de Novumideas" />
-            </figure>
-            <nav className="w-auto">
-                <ul className="flex justify-between gap-7 text-slate-50">
-                    <li>
-                        <a href="">Vacantes</a>
-                    </li>
-                    <li>
-                        <a href="">Nosotros</a>
-                    </li>
-                    <li>
-                        <a href="">Contacto</a>
-                    </li>
-                    <li>
-                        <a href="">LogIn</a>
-                    </li>
-                </ul>
-            </nav>
+        <section className = "flex w-full py-2.5 px-10 items-center justify-center" style={{ backgroundImage: `url('${imageNavBg}')` }}>
+            <div className= "flex w-full items-center justify-between px-5">
+                <figure className="w-32">
+                    <img src={novumLogo} alt="Logo de Novumideas" />
+                </figure>
+                <nav className="w-auto">
+                    <ul className="flex justify-between gap-7 text-slate-50">
+                        <li>
+                            <a href="">Vacantes</a>
+                        </li>
+                        <li>
+                            <a href="">Nosotros</a>
+                        </li>
+                        <li>
+                            <a href="">Contacto</a>
+                        </li>
+                        <li>
+                            <a href="">LogIn</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </section>
     )
 }
