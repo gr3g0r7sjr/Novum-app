b3681e4240a3c9b34f21625adac7ddaccd4c4d43
Gregory Subero <59521617+gr3g0r7sjr@users.noreply.github.com>
2025-06-19 20:14:29 +0000

    colocando los errors para el formulario

--- frontend/src/components/Auth/LoginAdmin.jsx
+++ frontend/src/components/Auth/LoginAdmin.jsx
@@ -3,8 +3,35 @@ import { useState } from "react";
 import styles from './LoginAdmin.module.scss'; 
 
 export const LoginAdmin = () => {
-    return(
+    const [formData, setFormData] = useState({
+        email: "", 
+        password: "",
+    });
 
+    const [errors, setErrors] = useState({});
+
+    function validate(){
+        const newErrors = {}; 
+        if(!formData.email) newErrors.email = "El email es obligatorio"; 
+        if(!formData.password) newErrors.password = "El password es obligatorio"; 
+        setErrors(newErrors); 
+        return Object.keys(newErrors).length === 0; 
+    }
+
+    function handleChange(e){
+        const {email, value} = e.target; 
+        setFormData({[email]: value, ...formData
+        }); 
+    }
+
+    function handleSubmit(e){
+        e.preventDefault(); 
+        if(validate()){
+            alert("Formulario enviado con éxito")
+        }
+    }
+
+    return(
         <>
             <section className = {styles.loginContainer}>
                 <div className = {styles.formContainer}>
@@ -12,16 +39,18 @@ export const LoginAdmin = () => {
                         <h2 className= {styles.title}>Bienvenido</h2>
                         <p className = {styles.p}>Ingresa tus credenciales para acceder a tu cuenta</p>
                     </div>
-                    <form action="" className = {styles.form}>
+                    <form action="" onSubmit = {handleSubmit} className = {styles.form}>
                         <label htmlFor="" className = {styles.label}>Correo electrónico</label>
+                        {errors.email && <p style = {{color: "red"}}>{errors.email}</p>}
                         <div className = {styles.inputContainer}>
                             <svg className = {styles.svg}  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-mail"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z" /><path d="M3 7l9 6l9 -6" /></svg>
-                            <input type="email" name="" id="" placeholder = "Tu @email" className = {styles.input} />
+                            <input type="email" name="email" value = {formData.email} onChange = {handleChange} placeholder = "Tu @email" className = {styles.input} />
                         </div>
                         <label htmlFor="" className = {styles.label}>Contraseña</label>
+                        {errors.password && <p style = {{color: "red"}}>{errors.password}</p>}
                         <div className = {styles.inputContainer}>
                             <svg className = {styles.svg}  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-lock"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 13a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v6a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-6z" /><path d="M11 16a1 1 0 1 0 2 0a1 1 0 0 0 -2 0" /><path d="M8 11v-4a4 4 0 1 1 8 0v4" /></svg>
-                            <input type="password" name="" id="" placeholder = "Tu contraseña" className = {styles.input} />
+                            <input type="password" name="password" value = {formData.password} onChange = {handleChange} placeholder = "Tu contraseña" className = {styles.input} />
                         </div>
                         <button type="submit" className = {styles.buttonForm}>
                             Iniciar Sesion 
