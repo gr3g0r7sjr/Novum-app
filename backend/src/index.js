fa8e093c81910592beaa9be6cee10ddd5ea08f71
Greg-js1007 <suberog010@gmail.com>
2025-06-20 21:18:02 -0400

    conexion y variables

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,22 @@
 import express from 'express'
+import {config} from 'dotenv'
+import pg from 'pg'
+
+config()
 
 const app = express()
+const pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL, 
+    ssl: true
+})
 
 app.get('/', (req, res) => {
     res.send('Hola')
+})
+
+app.get('/ping', async (req, rest) =>{
+    const result = await pool.query('SELECT NOW()')
+    return rest.json(result.rows[0])
 })
 
 app.listen(3000)
