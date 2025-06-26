43f06b1ea1fe0ce2adc4fa65eef779a49dab2954
Gregory Subero <59521617+gr3g0r7sjr@users.noreply.github.com>
2025-06-26 17:53:56 +0000

    feat: haciendo peticion con axios al ednopint de user login

--- frontend/src/components/Auth/LoginAdmin.jsx
+++ frontend/src/components/Auth/LoginAdmin.jsx
@@ -1,35 +1,63 @@
-import React from 'react';
+
 import { useState } from "react";
-import styles from './LoginAdmin.module.scss'; 
+import styles from './LoginAdmin.module.scss';
+import axios, { AxiosHeaders } from 'axios';
+import { useNavigate } from 'react-router-dom'; 
 
 export const LoginAdmin = () => {
-    const [formData, setFormData] = useState({
-        email: "", 
-        password: "",
-    });
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [estadoLogin, setEstadoLogin] = useState('')
+
+    const navigate = useNavigate(); 
 
     const [errors, setErrors] = useState({});
 
     function validate(){
         const newErrors = {}; 
-        if(!formData.email) newErrors.email = "El email es obligatorio"; 
-        if(!formData.password) newErrors.password = "El password es obligatorio"; 
+        if(!email) newErrors.email = "El email es obligatorio"; 
+        if(!password) newErrors.password = "El password es obligatorio"; 
         setErrors(newErrors); 
         return Object.keys(newErrors).length === 0; 
     }
 
-    function handleChange(e){
-        const {email, value} = e.target; 
-        setFormData({[email]: value
-        }); 
+    const handleChange = (e) =>{ 
+        setEmail(e.target.value)
+        setPassword(e.target.value) 
     }
 
-    function handleSubmit(e){
+    const handleSubmit = async (e) => {
         e.preventDefault(); 
         if(validate()){
             alert("Formulario enviado con éxito")
         }
+
+        const credenciales = {
+            email: email,
+            password: password,
+        }
+        try {
+            const response  = await axios.post('https://novum-app.onrender.com/login', {
+              headers: {
+                'Content-Type': 'application/json'
+              },
+            }); 
+            if(response.data.success){
+                setEstadoLogin('Inicio de sesion exitoso!')
+                navigate('/frontend/src/pages/Admin.jsx')
+            }
+            else{
+                setEstadoLogin(`Error de inicio de sesion: ${response.data.message}`)
+            }
+        }catch (error) {
+            const errorMessage = error.response
+                ? error.response.data.message || 'Error desconocido del servidor.' // Mensaje del backend
+                : error.message; // Mensaje de error de red
+            console.error('Error al iniciar sesión:', error);
+            setEstadoLogin(`Error al iniciar sesión: ${errorMessage}`);
+        }
     }
+
 
     return(
         <>
@@ -44,19 +72,20 @@ export const LoginAdmin = () => {
                         {errors.email && <p style = {{color: "red"}}>{errors.email}</p>}
                         <div className = {styles.inputContainer}>
                             <svg className = {styles.svg}  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-mail"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z" /><path d="M3 7l9 6l9 -6" /></svg>
-                            <input type="email" name="email" value = {formData.email} onChange = {handleChange} placeholder = "Tu @email" className = {styles.input} />
+                            <input type="email" name="email" value = {email} onChange = {handleChange} placeholder = "Tu @email" className = {styles.input} />
                         </div>
                         <label htmlFor="" className = {styles.label}>Contraseña</label>
                         {errors.password && <p style = {{color: "red"}}>{errors.password}</p>}
                         <div className = {styles.inputContainer}>
                             <svg className = {styles.svg}  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-lock"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 13a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v6a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-6z" /><path d="M11 16a1 1 0 1 0 2 0a1 1 0 0 0 -2 0" /><path d="M8 11v-4a4 4 0 1 1 8 0v4" /></svg>
-                            <input type="password" name="password" value = {formData.password} onChange = {handleChange} placeholder = "Tu contraseña" className = {styles.input} />
+                            <input type="password" name="password" value = {password} onChange = {handleChange} placeholder = "Tu contraseña" className = {styles.input} />
                         </div>
                         <button type="submit" className = {styles.buttonForm}>
                             Iniciar Sesion 
                             <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-arrow-right"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l14 0" /><path d="M13 18l6 -6" /><path d="M13 6l6 6" />
                             </svg>
                         </button>
+                        {estadoLogin && <p style={{ marginTop: '15px', fontWeight: 'bold' }}>{estadoLogin}</p>}
                     </form>
                 </div>
             </section>
