7e35dc884d40e268e241ff28120eeade618c25f0
Gregory Subero <59521617+gr3g0r7sjr@users.noreply.github.com>
2025-06-27 19:23:58 +0000

    cambios en auth ruta de authController

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'; 
 const router = express.Router(); 
-import { authController } from '../controllers/authController';
+import { authController } from '../controllers/authController.js';
 
 router.post('/login', authController.login)
 
