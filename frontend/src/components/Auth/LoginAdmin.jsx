6c3330385e56118cf0f81dbbe810c709287fe093
Greg-js1007 <suberog010@gmail.com>
2025-06-29 14:19:33 -0400

    fix estado de los handlechange en authLogin

--- frontend/src/components/Auth/LoginAdmin.jsx
+++ frontend/src/components/Auth/LoginAdmin.jsx
@@ -21,8 +21,11 @@ export const LoginAdmin = () => {
         return Object.keys(newErrors).length === 0; 
     }
 
-    const handleChange = (e) =>{ 
+    const handleChangeEmail = (e) =>{ 
         setEmail(e.target.value)
+    }
+
+    const handleChangePassword = (e) => {
         setPassword(e.target.value) 
     }
 
@@ -34,13 +37,13 @@ export const LoginAdmin = () => {
 
         const credenciales = {
             email: email,
-            password: password,
+            password: password
         }
         try {
             const response  = await axios.post('https://novum-app.onrender.com/login', {
-              headers: {
+            headers: {
                 'Content-Type': 'application/json'
-              },
+            },
             }); 
             if(response.data.success){
                 setEstadoLogin('Inicio de sesion exitoso!')
@@ -52,7 +55,7 @@ export const LoginAdmin = () => {
         }catch (error) {
             const errorMessage = error.response
                 ? error.response.data.message || 'Error desconocido del servidor.' // Mensaje del backend
-                : error.message; // Mensaje de error de red
+                : error.message; 
             console.error('Error al iniciar sesión:', error);
             setEstadoLogin(`Error al iniciar sesión: ${errorMessage}`);
         }
@@ -72,13 +75,13 @@ export const LoginAdmin = () => {
                         {errors.email && <p style = {{color: "red"}}>{errors.email}</p>}
                         <div className = {styles.inputContainer}>
                             <svg className = {styles.svg}  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-mail"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z" /><path d="M3 7l9 6l9 -6" /></svg>
-                            <input type="email" name="email" value = {email} onChange = {handleChange} placeholder = "Tu @email" className = {styles.input} />
+                            <input type="email" name="email" value = {email} onChange = {handleChangeEmail} placeholder = "Tu @email" className = {styles.input} />
                         </div>
                         <label htmlFor="" className = {styles.label}>Contraseña</label>
                         {errors.password && <p style = {{color: "red"}}>{errors.password}</p>}
                         <div className = {styles.inputContainer}>
                             <svg className = {styles.svg}  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-lock"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 13a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v6a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-6z" /><path d="M11 16a1 1 0 1 0 2 0a1 1 0 0 0 -2 0" /><path d="M8 11v-4a4 4 0 1 1 8 0v4" /></svg>
-                            <input type="password" name="password" value = {password} onChange = {handleChange} placeholder = "Tu contraseña" className = {styles.input} />
+                            <input type="password" name="password" value = {password} onChange = {handleChangePassword} placeholder = "Tu contraseña" className = {styles.input} />
                         </div>
                         <button type="submit" className = {styles.buttonForm}>
                             Iniciar Sesion 
