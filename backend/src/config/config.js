343397f24c112afca873a61aa121e2cdf26a614a
Gregory Subero <59521617+gr3g0r7sjr@users.noreply.github.com>
2025-07-04 12:50:56 +0000

    cambios en el .env

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,8 +1,7 @@
-// backend/src/config/config.js
-export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+export const FRONTEND_URL  = process.env.FRONTEND_URL || "http://localhost:5173"
 
-export const DB_HOST = process.env.DB_HOST || "localhost";
-export const DB_PORT = process.env.DB_PORT || 5432;
-export const DB_DATABASE = process.env.DB_DATABASE || "novum_app_db";
-export const DB_USER = process.env.DB_USER || "postgres";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "rs85047928084765";
+export const DB_HOST = process.env.DB_HOST ||"localhost"
+export const DB_PORT = process.env.DB_PORT || 5432
+export const DB_DATABASE = process.env.DB_DATABASE || "dbnovum"
+export const DB_USER = process.env.DB_USER || "postgres"
+export const DB_PASSWORD = process.env.DB_PASSWORD ||"eladio1007"
