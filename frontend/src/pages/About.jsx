a1937340a1742ed9bb03c65896f63520b1d2b530
Gregory Subero <59521617+gr3g0r7sjr@users.noreply.github.com>
2025-07-04 20:16:19 +0000

    modificando aboutjsx

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -4,30 +4,16 @@ export const About = () => {
         <section className = "bg-orange-primary">
             <div>
                 <h1>Sobre Nosotros</h1>
+                <p>Conectamos talento exepcional con oportunidades extraordinarias desde hace 7 años</p>
             </div>
-            <div>
-            <h2>
-                Nuestra Historia
-            </h2>
-            <p>
-                La creación de Novumideas no fue solo el establecimiento de una empresa; fue el resultado del empujón que dan las circunstancias difíciles y el deseo compartido de marcar la diferencia. En medio de la incertidumbre y los desafíos económicos-sociales de Venezuela, el vacío de profesionales ante las complejas demandas del mercado venezolano y la certeza de apostar por el país, fueron las claves determinantes que llevaron a crear esta organización.
-            </p>
-            <p>
-                A lo largo de este tiempo, Novumideas ha experimentado un crecimiento y un camino de evolución, cimentado en la solidez del equipo, su capacidad de adaptación y una misión compartida. Durante este tiempo ha demostrado a sus clientes un trabajo de excelencia y un compromiso destacable, en un entorno variable para el empresariado del país.
-            </p>
-            <p>
-                Nuestros líderes desde su fundación siempre han mirado a Venezuela como un mar lleno de oportunidades, han sido y siguen siendo visionarios y estrategas en la unión de fuerzas, en la incorporación de profesionales que también apuestan por hacer país. Bajo esa premisa, Novumideas no solo ha superado desafíos técnicos y comerciales, sino que ha cultivado un equipo unido, comprometido y resiliente. La empresa se distingue por su responsabilidad, integridad y ética empresarial, valores que han permeado en cada proyecto, requerimiento y solución brindada.
-            </p>
-            <p>
-                En el camino de la evolución de Novum, nació Sirio, una suite financiera concebida para revolucionar el panorama tecnológico bancario venezolano, este producto está motivado por el deseo de crear la mejor solución adaptada al día a día de la Banca, con versatilidad, seguridad y vanguardia.
-            </p>
-            <p>
-                Al mirar hacia el futuro, Novumideas se proyecta como una empresa líder en el desarrollo de soluciones tecnológicas que confirmen que los proyectos se hacen realidad con disciplina, responsabilidad y un compromiso Novum inquebrantable.
-            </p>
-            </div>
-            <div>
-                <button></button>
-            </div>
+            
+            <section>
+                <div>
+                </div>   
+                <div>
+                
+                </div>   
+            </section>
         </section>
     )
 }
