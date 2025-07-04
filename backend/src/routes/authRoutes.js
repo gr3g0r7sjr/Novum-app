3fbc82554b500e1c848a0ad0b984b03f465ef33b
Gregory Subero <59521617+gr3g0r7sjr@users.noreply.github.com>
2025-07-04 13:13:13 +0000

    arreglando el auth routes

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,39 +1,7 @@
-import { Usuario } from '../models/User.js'; 
-import bcrypt from 'bcryptjs';
+import express from 'express'; 
+const router = express.Router(); 
+import { authController } from '../controllers/authController.js';
 
-export const authController = {
-    login: async (req, res) => {
-        const {email, password} = req.body; 
+router.post('/login', authController.login)
 
-        if(!email || !password){
-            return res.status(400).json({success: false, message: 'Correo y contraseña son obligatorios'})
-        }
-
-        try {
-            const user = await Usuario.findByEmail(email); 
-
-            if(!user){
-                return res.status(401).json({success: false, message: 'Credenciales inválidas.'})
-            }
-
-            const passwordMatch = await bcrypt.compare(password, user.password); 
-
-            if(!passwordMatch){
-                return res.status(401).json({ success: false, message: 'Credenciales inválidas.' })
-            }
-
-            res.status(200).json({
-                success: true, 
-                message:'Inicio de Sesion exitoso',
-                user:{id: user.id, email: user.email}
-            })
-        } catch (error) {
-            console.error('Error en authController.login', error); 
-            res.status(500).json({
-                success: false,
-                message: 'Error interno del servidor al intentar iniciar sesión.',
-                error: error.message
-            })
-        }
-    }
-}
+export default router; 
