f24043798db891db1bc629d230ea6134fb19b920
Greg-js1007 <suberog010@gmail.com>
2025-07-04 23:59:19 -0400

    eliminando rutas de crearvacantes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,17 +7,11 @@ import { Home } from './pages/Home.jsx';
 import { About } from './pages/About.jsx'; // <--- ¡Aquí está la corrección!
 import { Contact } from './pages/Contact.jsx';
 import { LoginAdmin } from './components/Auth/LoginAdmin.jsx';
-import VacantesPage from './pages/VacantesPage.jsx';
-
-// Importa el nuevo componente del formulario
-import CrearVacantePage from './pages/CrearVacantePage.jsx'; 
-
 
 // Definición de diferentes GRUPOS de rutas de navegación
 const navRouteGroups = {
   main: [ // Rutas para la navegación principal (no admin)
     { path: '/', name: 'Vacantes' },
-    { path: '/crear-vacante', name: 'Crear Vacante' }, // Nueva ruta para el formulario
     { path: '/nosotros', name: 'Nosotros' },
     { path: '/contacto', name: 'Contacto' },
     { path: '/login', name: 'Login'}
@@ -32,7 +26,6 @@ function App() {
       <section>
         <Routes>
           <Route path="/" element={<Home />} /> 
-          <Route path="/crear-vacante" element={<CrearVacantePage />} />
           <Route path="/nosotros" element={<About />} />
           <Route path="/contacto" element={<Contact />} />
           <Route path='/login' element={<LoginAdmin />} />
