4cd864f110e2b735a3bbbb1f41c82ad48f95ed0b
Greg-js1007 <suberog010@gmail.com>
2025-07-12 18:55:57 -0400

    cards de mision y vision about

---
+++ frontend/src/components/CardsAbout/CardsMisionVision.jsx
@@ -0,0 +1,22 @@
+import styles from './CardsMisionVision.module.scss'; 
+
+export const CardsMisionVision = (props) =>{
+
+    const {icono, titulo, texto} = props
+
+    return (
+        <div className={styles.card_Mision_Vision}>
+            <div className={styles.mision}>
+                {icono}
+                <p>
+                    {titulo}
+                </p>
+            </div>
+            <div className={styles.vision}>
+                <p>
+                    {texto}
+                </p>
+            </div>
+        </div>
+    )
+}
--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,5 +1,7 @@
 import styles from '../styles/About.module.scss';
 import { CardsAboutUs } from '../components/CardsAbout/CardsAbout';
+import { CardsMisionVision } from '../components/CardsAbout/CardsMisionVision';
+import { Goal, Eye } from 'lucide-react';
 
 
 export const About = () => {
@@ -22,6 +24,21 @@ export const About = () => {
         }
     ]
 
+    const cardsMisionVision = [
+        {
+            id:1,
+            icono: <Goal color='#ff8500' />, 
+            titulo: 'Nuestra Misión',
+            texto: 'Hacer de las instituciones financieras un buen negocio: competitivos, rapidos con nuevos productos y servicios a menores costos en cumplimiento de las regulaciones.' 
+        },
+        {
+            id:1,
+            icono: <Eye color='#003091' />, 
+            titulo: 'Nuestra Visión',
+            texto: 'Ser referencia, reconocidos en el sector financiero nacional por trabajar con los mejores talentos y clientes para impulsar nuestro mercado a nivel internacional.' 
+        }
+    ]
+
     return (
         <section className = {styles.containerAbout}>
             <div>
@@ -29,7 +46,7 @@ export const About = () => {
                 <h1>Sobre Nosotros</h1>
                 <p>Es más que una empresa; es la materialización de ideas de software, la búsqueda constante <br /> de la excelencia y la convicción de que cualquier proyecto, por ambisioso que sea, es viable y alcanzable</p>
             </div>
-            <section>
+            <section className={styles.containerInfo}>
                 <div className={styles.containerCardAbout}>
                     {
                     cards.map(card => (
@@ -39,6 +56,18 @@ export const About = () => {
                             texto = {card.texto}
                         />
                     ))
+                    }
+                </div>
+                <div className={styles.containerMision}>
+                    {
+                        cardsMisionVision.map(card => (
+                            <CardsMisionVision
+                                key={card.id}
+                                icono = {card.icono}
+                                titulo = {card.titulo}
+                                texto = {card.texto}
+                            />
+                        ))
                     }
                 </div>
             </section>
