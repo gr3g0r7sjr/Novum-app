a40be59008a36adc9e622fd788f81cd5a967abd9
Greg-js1007 <suberog010@gmail.com>
2025-07-15 14:15:04 -0400

    config db

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -2,7 +2,7 @@ export const FRONTEND_URL  = process.env.FRONTEND_URL || "http://localhost:5174"
 
 export const DB_HOST = process.env.DB_HOST || "localhost"
 export const DB_PORT = process.env.DB_PORT || 5432
-export const DB_DATABASE = process.env.DB_DATABASE || "novum_app_db"
+export const DB_DATABASE = process.env.DB_DATABASE || "dbnovum"
 export const DB_USER = process.env.DB_USER || "postgres"
 export const DB_PASSWORD = process.env.DB_PASSWORD ||"eladio1007"
 export const JWT_SECRET = process.env.JWT_SECRET || a3cd07afc87e5bda71f06fd5916c3d162ca4190dba3cfa2d2d6820c45351585b
--- backend/src/db.js
+++ backend/src/db.js
@@ -1,34 +1,39 @@
 import 'dotenv/config.js'
 import pg from 'pg'
-import { DB_HOST, DB_DATABASE, DB_USER, DB_PASSWORD, DB_PORT } from '../config/config.js'
+import {DB_HOST, DB_DATABASE, DB_USER, DB_PASSWORD, DB_PORT} from './config/config.js'
+const {Pool} = pg
 
-const { Pool } = pg
-
-const sslConfig = DB_HOST === 'localhost' ? false : { rejectUnauthorized: false }
+const sslConfig = DB_HOST === 'localhost' ? false : {
+    rejectUnauthorized: false
+}
 
 const pool = new Pool({
-  host: DB_HOST,
-  database: DB_DATABASE,
-  user: DB_USER,
-  password: DB_PASSWORD,
-  port: DB_PORT,
-  ssl: sslConfig
+    host: DB_HOST, 
+    database: DB_DATABASE, 
+    user:DB_USER, 
+    password: DB_PASSWORD, 
+    port: DB_PORT,
+    ssl: sslConfig
 })
 
+
 async function testDbConnection() {
-  let client
-  try {
-    client = await pool.connect()
-    console.log('✅ ¡Conectado a PostgreSQL exitosamente!')
-  } catch (err) {
-    console.error('❌ ERROR: No se pudo conectar a la base de datos:', err.stack)
-  } finally {
-    if (client) {
-      client.release()
+    let client; 
+    try {
+        client = await pool.connect(); // Adquiere un cliente del pool
+        console.log('¡Conectado a PostgreSQL exitosamente y pool listo!');
+    } catch (err) {
+        console.error('¡ERROR! No se pudo establecer la conexión inicial con la base de datos:', err.stack);
+
+    } finally {
+        if (client) {
+            client.release(); // IMPORTANTE: Libera el cliente de vuelta al pool
+            console.log('Cliente liberado al pool.');
+        }
     }
-  }
 }
 
 testDbConnection()
 
-export default pool
+
+export default pool; 
