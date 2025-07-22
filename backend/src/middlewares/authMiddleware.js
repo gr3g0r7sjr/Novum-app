83430ce9f081f2cb16aa64254c3a8af2b27e3b33
Greg-js1007 <suberog010@gmail.com>
2025-07-22 09:58:18 -0400

    comentando console.logs de middleware

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -6,7 +6,7 @@ const authenticateToken = (req, res, next) => {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
 
-    console.log('DEBUG AUTH: Token recibido:', token ? 'Sí' : 'No'); // <-- DEBUG
+    //console.log('DEBUG AUTH: Token recibido:', token ? 'Sí' : 'No'); // <-- DEBUG
     
     if (token == null) {
         return res.status(401).json({ message: 'Acceso denegado. No se proporcionó token de autenticación.' });
@@ -14,11 +14,11 @@ const authenticateToken = (req, res, next) => {
 
     jwt.verify(token, JWT_SECRET, (err, user) => {
         if (err) {
-            console.error('DEBUG AUTH: Error de verificación de token:', err.message); // <-- DEBUG
+            //console.error('DEBUG AUTH: Error de verificación de token:', err.message); // <-- DEBUG
             return res.status(403).json({ message: 'Token de autenticación inválido o expirado.' });
         }
         req.user = user;
-        console.log('DEBUG AUTH: Token verificado. req.user adjuntado:', req.user); // <-- DEBUG
+        //console.log('DEBUG AUTH: Token verificado. req.user adjuntado:', req.user); // <-- DEBUG
         next();
     });
 };
