0cef6eac707e2ec212e841f0d857483284a46761
Richard Sosa <rrichardsosa2209@gmail.com>
2025-07-22 23:11:06 -0400

    commit prueba

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,10 @@
-// backend/server.js (tu index.js)
-import 'dotenv/config'; // Carga las variables de entorno desde .env
-import {FRONTEND_URL, JWT_SECRET } from './config/config.js'; // Importa variables de configuración
+import 'dotenv/config'; 
+import {FRONTEND_URL, JWT_SECRET } from './config/config.js'; 
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import jwt from 'jsonwebtoken'; 
 
-// Importa los middlewares
 import authenticateToken from './middlewares/authMiddleware.js'; 
 
 
@@ -17,15 +15,13 @@ import { vacantesController } from './controllers/vacantesController.js';
 const app = express();
 const port = process.env.PORT || 3000; 
 
-// Configuración de CORS
 app.use(cors({
     origin: [FRONTEND_URL, 'http://localhost:5174', 'http://localhost:3000'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
-
-// Middlewares para parsear el cuerpo de las peticiones
+//Hola
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
