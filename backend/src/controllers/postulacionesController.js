8307c4ad2a2d8c92565e1d6db9e95a8af9e8477f
Greg-js1007 <suberog010@gmail.com>
2025-07-24 17:28:29 -0400

    postulaciones controller

--- backend/src/controllers/postulacionesController.js
+++ backend/src/controllers/postulacionesController.js
@@ -72,7 +72,7 @@ export const postulacionesController = {
                             servicio_interes, vehiculo
                         ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING id_candidato`,
             [
-              nombre,
+            nombre,
               apellido,
               correo_electronico,
               numero_telefono,
